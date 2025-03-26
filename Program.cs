ccfc591e7c803fbe53e42f93f0417d7fe86506da
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-26 08:55:14 +0530

    Lambda Expression

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lamda Expression
+using System;
+using System.Reflection.Metadata.Ecma335;
+namespace lambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(7));
+        }
+    }
+}
