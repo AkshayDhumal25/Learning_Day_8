ef48c8b6745c8c88a5fb09ef7239b85873667cd2
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-26 11:36:09 +0530

    Async and Await it executes the methods which takes less time to execute as compare to other methods and the parallel execution is done here.

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@
 //    {
 //        static void Main(string[] args)
 //        {
-            
+
 
 
 //            try
@@ -49,3 +49,94 @@
 //        }
 //    }
 //}
+
+// Async and Await
+//using System;
+//using System.Diagnostics;
+//namespace AsynchronousProgramming
+//{
+//    public class AsyncAwaitPart1
+//    {
+//        public static void Method1()
+//        {
+//            Thread.Sleep(3500);
+//            Console.WriteLine("Method1 Executed");
+//        }
+//        public static void Method2()
+//        {
+//            Thread.Sleep(3500);
+//            Console.WriteLine("Method2 Executed");
+//        }
+//        public static void Method3()
+//        {
+//            Thread.Sleep(3500);
+//            Console.WriteLine("Method3 Executed");
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            Stopwatch stopwatch = new Stopwatch();
+//            stopwatch.Start();
+//            Method1();
+//            Method2();
+//            Method3();
+//            stopwatch.Stop();
+//            var timeElapsed = stopwatch.ElapsedMilliseconds;
+//            Console.WriteLine("Time take for execution of all 3 methods :" + timeElapsed);
+//        }
+//    }
+//}
+//Method1 Executed
+//Method2 Executed                                        by Sync
+//Method3 Executed
+//Time take for execution of all 3 methods :10542
+
+
+
+
+//using Aync and Await Method
+//using System;
+//using System.Diagnostics;
+//namespace AsynchronousProgramming
+//{
+//    public class AsyncAwaitPart1
+//    {
+//        public static async Task Method1()
+//        {
+//            await Task.Delay(7000);
+//            //Thread.Sleep(3500);
+//            Console.WriteLine("Method1 Executed");
+//        }
+//        public static async Task  Method2()
+//        {
+//            await Task.Delay(3500);
+//            //Thread.Sleep(3500);
+//            Console.WriteLine("Method2 Executed");
+//        }
+//        public static async Task Method3()
+//        {
+//            await Task.Delay(9000);
+//            //Thread.Sleep(3500);
+//            Console.WriteLine("Method3 Executed");
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            Stopwatch stopwatch = new Stopwatch();
+//            stopwatch.Start();
+//            Task.WaitAll(Method1(), Method2(), Method3());
+//            //Method1();
+//            //Method2();
+//            //Method3();
+//            stopwatch.Stop();
+//            var timeElapsed = stopwatch.ElapsedMilliseconds;
+//            Console.WriteLine("Time take for execution of all 3 methods :" + timeElapsed);
+//        }
+//    }
+//}
+//Method2 Executed
+//Method1 Executed
+//Method3 Executed
+//Time take for execution of all 3 methods :9018
+
+
