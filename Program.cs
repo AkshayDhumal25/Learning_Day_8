d1fef5e2ec21b091bd80a469d85efe91357cd59f
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-26 15:09:16 +0530

    Runtime and CompileTime Ploymorphism

--- Program.cs
+++ Program.cs
@@ -139,4 +139,304 @@
 //Method3 Executed
 //Time take for execution of all 3 methods :9018
 
+// Logicals
+//4.Find the reverse of the string.
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string str = "Akshay";
+//            string reverse = "";
+//            for(int i = str.Length - 1; i >= 0; i--)
+//            {
+//                reverse += str[i];
+//            }
+//            Console.WriteLine(reverse);
 
+//        }
+//    }
+//}
+
+
+//5. WAP to find missing elements from the array?
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = { 1, 2, 3, 5, 6 };
+//            int n = arr.Length + 1;
+//            int totalSum = (n * (n + 1) / 2);
+//            int currSum = 0;
+//            foreach(int i in arr)
+//            {
+//                currSum += i;
+//            }
+
+//            Console.WriteLine(totalSum - currSum);
+//        }
+//    }
+//}
+
+//6.WAP to find the given string is Palindrome or not.
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string str = "abbaa";
+//            int left = 0;
+//            int right = str.Length - 1;
+//            bool palindrome = true;
+//            while(left < right)
+//            {
+//                if (str[left] == str[right])
+//                {
+//                    //continue;
+//                    left++;
+//                    right--;
+//                    palindrome = true;
+//                }
+//                else
+//                {
+//                    palindrome = false;
+//                }
+//            }
+
+//            if (palindrome)
+//            {
+//                Console.WriteLine("Palindrome");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Not Palindrome");
+//            }
+//        }
+//    }
+//}
+
+
+//7.WAP to print Fibonacci series with recursion.
+//using System;
+//namespace Logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var n = Convert.ToInt32(Console.ReadLine());
+
+//            PrintFibonacciSeries(n);
+
+
+//        }
+
+//        static void PrintFibonacciSeries(int n)
+//        {
+//            for (int i = 1; i <= n; i++)
+//            {
+//                Console.Write(Fibonacci(i) + " ");
+//            }
+//        }
+//        public static int Fibonacci(int n)
+//        {
+//            if (n <= 0)
+//            {
+//                Console.WriteLine("Input must be a positive integer.");
+//                return -1;
+//            }
+//            else if (n == 1)
+//                return 0;
+//            else if (n == 2)
+//                return 1;
+//            else
+//                return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+//        }
+//    }
+//}
+
+
+//8. WAP to print Fibonacci series without recursion.
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int n = int.Parse(Console.ReadLine());
+//            Fibonacci(n);
+//        }
+
+
+//        public static void Fibonacci(int n)
+//        {
+//            int a = 0;
+//            int b = 1;
+//            int c = 0;
+//            Console.Write(a + " " + b + " ");
+//            for (int i = 2; i < n; i++) // Start from index 2
+//            {
+//                c = a + b;
+//                Console.Write(c + " "); // Print next Fibonacci number
+//                a = b;
+//                b = c; // Update for next iteration
+//            }
+//        }
+//    }
+//}
+
+
+//10.Code to find even numbers in a list
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+
+//            var list = new List<int>() {1,2,3,4,5,6,7 };
+//            for(int i = 0; i < list.Count; i++)
+//            {
+//                if (isPrime(list[i]))
+//                {
+//                    Console.Write(list[i] + " ");
+//                }
+//                else
+//                {
+//                    Console.Write("N" + " ");
+//                }
+//            }
+//            //Console.WriteLine("Enter the number : ");
+//            //int n = Convert.ToInt32(Console.ReadLine());
+//            //if (isPrime(n))
+//            //{
+//            //    Console.WriteLine("Prime Number");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("Not Prime Number");
+//            //}
+//        }
+//        public static bool isPrime(int n)
+//        {
+//            if (n < 2) return false;
+//            if (n == 2) return true;
+//            if (n % 2 == 0) return false;
+
+//            for(int i=3; i * i < n; i++)
+//            {
+//                if (n % i == 0) return false;
+//            }
+//            return true;
+
+//        }
+//    }
+//}
+
+// Generics
+//using System;
+//namespace generics 
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter the X value to swap");
+//            var x = Console.ReadLine();
+//            Console.WriteLine("Enter the Y value");
+//            var y = Console.ReadLine();
+//            swap(x, y);
+
+
+//        }
+
+//        public static void swap<T> (T x, T y)
+//        {
+//            var temp = x;
+//            x = y;
+//            y = temp;
+
+//            Console.WriteLine($"X : {x} and Y : {y}");
+//            Console.ReadLine();
+//        }
+//    }
+
+//}
+
+// Method Overloading and Method Overrinding
+//using System;
+//namespace methodOcerloading
+//{
+//    class MathOperations
+//    {
+//        public int add(int x , int y)
+//        {
+//            return x + y;
+//        }
+//        public int add(int x, int y, int z)
+//        {
+//            return x + y + z;
+//        }
+//        public double add(double x, double y)
+//        {
+//            return x + y;
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            MathOperations ops = new MathOperations();
+//            Console.WriteLine(ops.add(1, 2));
+//            Console.WriteLine(ops.add(1, 2,3));
+//            Console.WriteLine(ops.add(1.0, 2.5));
+
+//        }
+//    }
+//}
+
+
+// Method Overriding
+using System;
+namespace methodOverriding
+{
+
+    class A
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Class A");
+        }
+    }
+
+    class B : A
+    {
+        public override void show()
+        {
+            Console.WriteLine("class B");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.show();
+
+            B b = new B();
+            b.show();
+
+            
+        }
+    }
+}
